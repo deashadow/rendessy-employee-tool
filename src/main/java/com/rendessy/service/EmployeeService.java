51aa370bccbc03835ae07bc27e60dc8d36f7c33e
deashadow <67285924+deashadow@users.noreply.github.com>
2021-06-11 15:38:22 -0400

    corrected pom.xml to include the thymeleaf dependency

--- src/main/java/com/rendessy/service/EmployeeService.java
+++ src/main/java/com/rendessy/service/EmployeeService.java
@@ -3,10 +3,7 @@ package com.rendessy.service;
 import java.util.List;
 
 import com.rendessy.model.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Page;
+
 
 
 
@@ -15,5 +12,5 @@ public interface EmployeeService {
         void saveEmployee (Employee employee);
         Employee getEmployeeById ( long id);
         void deleteEmployeeById ( long id);
-        Page<Employee> findPaginated ( int pageNo, int pageSize, String sortField, String sortDirection);
+        //Page<Employee> findPaginated ( int pageNo, int pageSize, String sortField, String sortDirection);
     }
--- src/main/java/com/rendessy/service/EmployeeServiceImpl.java
+++ src/main/java/com/rendessy/service/EmployeeServiceImpl.java
@@ -6,13 +6,6 @@ import java.util.Optional;
 import com.rendessy.model.Employee;
 import com.rendessy.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,9 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class EmployeeServiceImpl implements EmployeeService {
 
+    @Autowired
     private  EmployeeRepository employeeRepository;
 
-    @Autowired
+
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
@@ -53,14 +47,5 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public void deleteEmployeeById(long id) {
         this.employeeRepository.deleteById(id);
-    }
-
-    @Override
-    public Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
-        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
-                Sort.by(sortField).descending();
-
-        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
-        return this.employeeRepository.findAll(pageable);
     }
 }
